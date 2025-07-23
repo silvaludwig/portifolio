4ed2c2149f3226f6b5a1b68f67639bd6fc7255ba
Ludwig S <silvaludwig@outlook.com>
2025-07-23 13:52:33 -0300

    tentando servir as imagens com static

--- meu_site/meu_site/settings.py
+++ meu_site/meu_site/settings.py
@@ -111,6 +111,7 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'meu_site/static')]  # adicione esta linha
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
