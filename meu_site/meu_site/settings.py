b482021096df5f4218983f9138b7e387e1a6852c
Ludwig S <silvaludwig@outlook.com>
2025-07-23 12:07:50 -0300

    deploy de novo

--- meu_site/meu_site/settings.py
+++ meu_site/meu_site/settings.py
@@ -39,7 +39,9 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
+
 
 ROOT_URLCONF = 'meu_site.urls'
 
