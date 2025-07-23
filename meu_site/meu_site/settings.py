e827199674aee7f08b8a27fe50f35a33430adb62
Ludwig S <silvaludwig@outlook.com>
2025-07-23 14:07:28 -0300

    ainda tentando fazer os static serem servidos no deploy

--- meu_site/meu_site/settings.py
+++ meu_site/meu_site/settings.py
@@ -104,13 +104,13 @@ USE_I18N = True
 
 USE_TZ = True
 
-# MEDIA_URL = '/media/'
-# MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'meu_site/static')]  # adicione esta linha
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
